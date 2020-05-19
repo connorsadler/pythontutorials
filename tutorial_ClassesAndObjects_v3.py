b4814cc9c7de429727ccd02bfc9f055334e93b8c
connors <connor.sadler@gmail.com>
2020-05-19 21:20:21 +0100

    next few code versions

---
+++ tutorial_ClassesAndObjects_v3.py
@@ -0,0 +1,49 @@
+import random
+
+class Player():
+	def __init__(self, name, colour):
+		self.name = name
+		self.colour = colour
+
+player1 = Player("Connor", "Red")
+player2 = Player("John", "Blue")
+
+currentPlayer = 1
+hiddenNumber = random.randint(1,100)
+print("OK I’m thinking of a number - please take turns to try to guess it")
+print("Players in the game are:")
+print("Player 1 is: " + player1.name + " with colour: " + player1.colour)
+print("Player 2 is: " + player2.name + " with colour: " + player2.colour)
+while True:
+	print("Player " + str(currentPlayer) + " turn")
+	if (currentPlayer == 1):
+		currentPlayerName = player1.name 
+		currentPlayerColour = player1.colour
+	else:
+		currentPlayerName = player2.name 
+		currentPlayerColour = player2.colour
+ 
+	print("Player " + currentPlayerColour + ", please take your turn " + currentPlayerName)
+ 
+	# Allow player to choose their number
+	playerChoice = int(input("Choose your number from 1-100: "))
+	print("You chose: " + str(playerChoice))
+
+	# check player’s choice against hiddenNumber
+	if playerChoice == hiddenNumber:
+		print("Correct! You won the game")
+		break
+	else:
+		message = "higher than" if hiddenNumber > playerChoice else "lower than"
+		print("That's not right. The number I'm thinking of is " + message + " your number")
+	
+	if currentPlayer == 1:
+		currentPlayer = 2 
+	else:
+		currentPlayer = 1
+
+# The winner will be currentPlayer
+if (currentPlayer == 1):
+	print("The winner is Player 1: " + player1.name + " with colour " + player1.colour)
+else:
+	print("The winner is Player 2: " + player2.name + " with colour " + player2.colour)
