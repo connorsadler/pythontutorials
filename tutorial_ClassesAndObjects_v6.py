49af3879ec3d8c9920f6d55eb06c0b52df17601e
connors <connor.sadler@gmail.com>
2020-05-23 10:48:41 +0100

    v6 of code

---
+++ tutorial_ClassesAndObjects_v6.py
@@ -0,0 +1,43 @@
+import random
+
+class Player():
+	def __init__(self, number, name, colour):
+		self.number = number
+		self.name = name
+		self.colour = colour
+
+	def getDescription(self):
+		return self.name + " with colour: " + self.colour
+
+player1 = Player(1, "Connor", "Red")
+player2 = Player(2, "John", "Blue")
+
+currentPlayer = player1
+hiddenNumber = random.randint(1,100)
+print("OK I’m thinking of a number - please take turns to try to guess it")
+print("Players in the game are:")
+print("Player 1 is: " + player1.getDescription())
+print("Player 2 is: " + player2.getDescription())
+while True:
+	print("Player " + str(currentPlayer.number) + " turn")
+	print("Player " + currentPlayer.colour + ", please take your turn " + currentPlayer.name)
+ 
+	# Allow player to choose their number
+	playerChoice = int(input("Choose your number from 1-100: "))
+	print("You chose: " + str(playerChoice))
+
+	# check player’s choice against hiddenNumber
+	if playerChoice == hiddenNumber:
+		print("Correct! You won the game")
+		break
+	else:
+		message = "higher than" if hiddenNumber > playerChoice else "lower than"
+		print("That's not right. The number I'm thinking of is " + message + " your number")
+	
+	if currentPlayer == player1:
+		currentPlayer = player2
+	else:
+		currentPlayer = player1
+
+# The winner will be currentPlayer
+print("The winner is: " + currentPlayer.getDescription())
